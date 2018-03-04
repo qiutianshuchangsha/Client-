01380a558cd9b8b397643ecd3f3985da0f691809
qiutianshu <1270566865@qq.com>
2018-03-04 09:39:26 +0800

    Initial commit

---
+++ Client/ClientDlg.h
@@ -0,0 +1,41 @@
+
+// ClientDlg.h : 头文件
+//
+
+#pragma once
+#include "afxcmn.h"
+#include "afxwin.h"
+
+
+// CClientDlg 对话框
+class CClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CClientDlg(CWnd* pParent = NULL);	// 标准构造函数
+//	CClientDlg *m_clientdlg;
+
+// 对话框数据
+	enum { IDD = IDD_CLIENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_list;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	CButton m_btn_start;
+	CButton m_btn_stop;
+};
